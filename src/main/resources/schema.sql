b5ad0e011f95a9adf38ed19c9a50a205678dd7ae
sandeep4396 <sandeep.sagar033@gmail.com>
2026-02-07 17:56:41 -0600

    Setup DB

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS `customers` (
+    `customer_id` int AUTO_INCREMENT PRIMARY KEY,
+    `first_name` varchar(100) NOT NULL,
+    `last_name` varchar(100) NOT NULL,
+    `email` varchar(255) NOT NULL UNIQUE,
+    `mobile_number` varchar(20) NOT NULL UNIQUE,
+    `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    `created_by` varchar(100) NOT NULL,
+    `updated_by` varchar(100) DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `accounts` (
+    `account_number` int AUTO_INCREMENT PRIMARY KEY,
+    `customer_id` int NOT NULL,
+    `account_type` varchar(100) NOT NULL,
+    `branch_address` varchar(255) NOT NULL,
+    `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `created_by` varchar(100) NOT NULL,
+    `updated_by` varchar(100) DEFAULT NULL,
+);
