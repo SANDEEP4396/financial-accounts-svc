fd3283f10aba912b84779946cabcf66a09e8710f
sandeep4396 <sandeep.sagar033@gmail.com>
2026-02-09 20:26:26 -0600

    Add validation annotations to DTOs and enhance global exception handling

--- src/main/java/com/financial/microservices/exception/GlobalExceptionHandler.java
+++ src/main/java/com/financial/microservices/exception/GlobalExceptionHandler.java
@@ -1,17 +1,37 @@
 package com.financial.microservices.exception;
 
 import com.financial.microservices.dto.ErrorResponseDTO;
+import org.jspecify.annotations.NonNull;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @SuppressWarnings("unused")
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDTO> handleGlobalException(final Exception ex, final WebRequest request) {
+        ErrorResponseDTO errorResponse = new ErrorResponseDTO(
+                ex.getMessage(),
+                request.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                LocalDateTime.now()
+        );
+                return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
     @ExceptionHandler(CustomerDetailsAlreadyExist.class)
     public ResponseEntity<ErrorResponseDTO> handleCustomerDetailsAlreadyExist(final CustomerDetailsAlreadyExist ex,
@@ -43,5 +63,23 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(errorResponse);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(final MethodArgumentNotValidException ex,
+                                     final @NonNull HttpHeaders headers,
+                                     final @NonNull HttpStatusCode statusCode,
+                                     final WebRequest request) {
+        final Map<String, String> errors = new HashMap<>();
+        List<ObjectError> validationErrors = ex.getBindingResult().getAllErrors();
+        validationErrors.forEach(error -> {
+            String fieldName = ((org.springframework.validation.FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errors);
     }
 }
