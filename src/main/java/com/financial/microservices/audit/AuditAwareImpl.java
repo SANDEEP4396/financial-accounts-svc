5e9917a2d3ce0faa2b4528ea4238c0979e084fb2
sandeep4396 <sandeep.sagar033@gmail.com>
2026-02-09 20:33:05 -0600

    Add validation annotations to DTOs and enhance global exception handling

---
+++ src/main/java/com/financial/microservices/audit/AuditAwareImpl.java
@@ -0,0 +1,16 @@
+package com.financial.microservices.audit;
+
+import org.jspecify.annotations.NullMarked;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("auditorAwareImpl")
+public class AuditAwareImpl implements AuditorAware<String> {
+    @Override
+    @NullMarked
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("Accounts_Svc"); // In a real application, you would return the actual user performing the operation, e.g., from a security context.
+    }
+}
