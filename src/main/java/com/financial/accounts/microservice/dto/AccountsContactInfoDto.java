c5e4972c07f88cfcae72b5e1051c6fe5cdc5618c
sandeep4396 <sandeep.sagar033@gmail.com>
2026-02-27 15:52:10 -0600

    Configure rabbitMQ

--- src/main/java/com/financial/accounts/microservice/dto/AccountsContactInfoDto.java
+++ src/main/java/com/financial/accounts/microservice/dto/AccountsContactInfoDto.java
@@ -11,8 +11,11 @@ import java.util.Map;
 @Getter
 @Setter
 public class AccountsContactInfoDto {
-
     private String message;
     private Map<String, String> contactDetails;
     private List<String> onCallSupport;
 }
+
+// If we make this a record class we will not be able to use @ConfigurationProperties to bind the properties from the application.yml file to this class.
+// This is because record classes are immutable and do not have setters, which are required for @ConfigurationProperties to work.
+// Therefore, we need to use a regular class with getters and setters to allow Spring Boot to bind the properties correctly.
