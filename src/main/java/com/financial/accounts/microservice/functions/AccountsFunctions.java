7f3aabc96058538b0663b75655db0b1637738bff
sandeep4396 <sandeep.sagar033@gmail.com>
2026-04-17 19:30:19 -0500

    Setup kafka

--- src/main/java/com/financial/accounts/microservice/functions/AccountsFunctions.java
+++ src/main/java/com/financial/accounts/microservice/functions/AccountsFunctions.java
@@ -1,7 +1,6 @@
 package com.financial.accounts.microservice.functions;
 
 import com.financial.accounts.microservice.service.IAccountsService;
-import com.financial.accounts.microservice.service.impl.AccountsServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
